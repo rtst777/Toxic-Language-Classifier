40bd7d132419d8b45171a554dbd68bf74e13abb6
EEEEEEEhan7 <yizhan.jiang@gmail.com>
2019-03-03 20:01:21 -0500

    applied data cleaning for the tweets

--- data/data_converter.py
+++ data/data_converter.py
@@ -1,23 +1,50 @@
 import pandas as pd
+import re
+import string
+import nltk
 
 github_data_raw_file_name = "github/labeled_data.csv"
 github_data_cleaned_file_name = "cleaned_data/dataset1.csv"
+# nltk.download()
+stopword = nltk.corpus.stopwords.words('english')
 
-def one_hot_encoding(dataframe, labels):
-    for target_label_index in range(len(labels)):
-        for i in range(len(labels)):
-            label = labels[i]
-            if i == target_label_index:
-                dataframe.loc[dataframe['class'] == target_label_index, label] = 1
-            else:
-                dataframe.loc[dataframe['class'] == target_label_index, label] = 0
+def remove_punct(text):
+    text  = "".join([char for char in text if char not in string.punctuation])
+    text = re.sub('[0-9]+', '', text)
+    return text
+
+def tokenization(text):
+    text = re.split('\W+', text)
+    return text
+
+def remove_stopwords(text):
+    text = [word for word in text if word not in stopword]
+    return text
+
+def remove_empty_string_token(text):
+    text = list(filter(None, text))
+    return text
+
+def lower_case_text(text):
+    text = [x.lower() for x in text]
+    return text
 
 def convert_github_data():
     df = pd.read_csv(github_data_raw_file_name)
-    subdf = df[['hate_speech', 'offensive_language', 'neither', 'class', 'tweet']]
-    labels = ['hate_speech', 'offensive_language', 'neither']
-    one_hot_encoding(subdf, labels)
-    subdf[['hate_speech', 'offensive_language', 'neither', 'tweet']].to_csv(path_or_buf=github_data_cleaned_file_name)
+    subdf = df[['tweet', 'class']]
+    subdf.columns = ['data', 'label'] # 0: hate speech, 1: offensive language, 2: neither
+
+    # clean tweet data
+    # https://www.kaggle.com/ragnisah/text-data-cleaning-tweets-analysis
+    subdf['data'] = subdf['data'].apply(lambda x: remove_punct(x))
+    subdf['data'] = subdf['data'].apply(lambda x: tokenization(x))
+    subdf['data'] = subdf['data'].apply(lambda x: remove_stopwords(x))
+    subdf['data'] = subdf['data'].apply(lambda x: remove_empty_string_token(x))
+    subdf['data'] = subdf['data'].apply(lambda x: lower_case_text(x))
+
+    # save cleaned data to file
+    subdf.to_csv(path_or_buf=github_data_cleaned_file_name)
+
 
 # TODO
 def convert_kaggle_data():
