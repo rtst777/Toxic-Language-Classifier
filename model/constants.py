e9dfca13054fce884e2430ef2354cd3be212ecc5
EEEEEEEhan7 <yizhan.jiang@gmail.com>
2019-03-16 18:49:34 -0400

    uploaded trained result

--- model/constants.py
+++ model/constants.py
@@ -1,3 +1,5 @@
-github_data_clean_data = "../data/cleaned_data/dataset1.csv"
+github_cleaned_data = "../data/cleaned_data/dataset1.csv"
+kaggle_cleaned_train_data = "../data/cleaned_data/dataset2.csv"
+kaggle_cleaned_test_data = "../data/cleaned_data/dataset3.csv"
 GLOVE_INPUT_SIZE = 50
 FAST_TEXT_INPUT_SIZE = 300
--- model/train_model.py
+++ model/train_model.py
@@ -10,9 +10,10 @@ import codecs
 from model.baseline_model import ToxicBaseLSTM
 from model.fasttext_based_lstm_model import FastTextBasedLSTMModel
 from model.glove_based_lstm_model import GloveBasedLSTMModel
-from model.constants import github_data_clean_data
+from model.constants import github_cleaned_data, kaggle_cleaned_train_data, kaggle_cleaned_test_data
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
 
 
 def set_global_seed(seed=37):
@@ -88,7 +89,7 @@ def train_model(model, train_set, valid_set, batch_size = 32, learning_rate = 0.
         # Save the current model (checkpoint) to a file
         model_path = get_model_name(model.name, batch_size, learning_rate, epoch, momentum)
         torch.save(model.state_dict(), model_path)
-    np.savetxt("{}_train_err.csv".format(model_path), train_err)
+    np.savetxt("{}_train_err.csv".format(model_path), train_err)  # FIXME  err should be accuracy
     np.savetxt("{}_train_loss.csv".format(model_path), train_loss)
     np.savetxt("{}_val_err.csv".format(model_path), val_err)
     np.savetxt("{}_val_loss.csv".format(model_path), val_loss)
@@ -101,6 +102,22 @@ def tokenizer(word_str):
     # return " ".join(list_of_word)
     # return word_str
     # return convert_word_to_glove(list_of_word)
+
+def balance_data_set(data_set):
+    # save the original training examples
+    old_examples = data_set.examples
+
+    train_non_toxic_example = []
+    for item in data_set.examples:
+        if item.label == 2:
+            train_non_toxic_example.append(item)
+
+    # duplicate non-toxic example
+    total_num_example = len(old_examples)
+    total_num_non_toxic_example = len(train_non_toxic_example)
+    scale_factor = (total_num_example // total_num_non_toxic_example) - 1
+
+    data_set.examples = old_examples + train_non_toxic_example * scale_factor
 
 index_to_vocab = None
 def create_dataloader():
@@ -116,11 +133,20 @@ def create_dataloader():
                                        preprocessing=lambda x: int(x))  # convert text to 0 and
     fields = [('data', word_field), ('label', label_field)]
 
-    dataset = torchtext.data.TabularDataset(path=github_data_clean_data, skip_header=True, format='csv', fields=fields)
-    train_set, valid_set, test_set = split_data(dataset)
+    dataset1 = torchtext.data.TabularDataset(path=github_cleaned_data, skip_header=True, format='csv', fields=fields)
+    # TODO concatenate three datasets
+    # dataset2 = torchtext.data.TabularDataset(path=kaggle_cleaned_train_data, skip_header=True, format='csv', fields=fields)
+    # dataset3 = torchtext.data.TabularDataset(path=kaggle_cleaned_test_data, skip_header=True, format='csv', fields=fields)
+
+    train_set, valid_set, test_set = split_data(dataset1)
+
+    # create vocabulary index
     word_field.build_vocab(train_set)
     global index_to_vocab
     index_to_vocab = word_field.vocab.itos
+
+    # balance training set data
+    balance_data_set(train_set)
 
     return train_set, valid_set, test_set
 
@@ -128,6 +154,32 @@ def create_dataloader():
 def split_data(dataset):
     train_set, valid_set, test_set = dataset.split([0.6, 0.2, 0.2], random_state=random.getstate())
     return train_set, valid_set, test_set
+
+def plot_training_curve(path):
+    """ Plots the training curve for a model run, given the csv files
+    containing the train/validation error/loss.
+
+    Args:
+        path: The base path of the csv files produced during training
+    """
+    train_err = np.loadtxt("{}_train_err.csv".format(path))
+    val_err = np.loadtxt("{}_val_err.csv".format(path))
+    train_loss = np.loadtxt("{}_train_loss.csv".format(path))
+    val_loss = np.loadtxt("{}_val_loss.csv".format(path))
+    plt.title("Train vs Validation Accuracy")
+    plt.plot(range(1,31), train_err, label="Train")
+    plt.plot(range(1,31), val_err, label="Validation")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.legend(loc='best')
+    plt.show()
+    plt.title("Train vs Validation Loss")
+    plt.plot(range(1,31), train_loss, label="Train")
+    plt.plot(range(1,31), val_loss, label="Validation")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend(loc='best')
+    plt.show()
 
 
 if __name__== "__main__":
@@ -137,12 +189,8 @@ if __name__== "__main__":
     # model = GloveBasedLSTMModel(index_to_vocab=index_to_vocab)
     # train_model(model, train_set, valid_set, batch_size=32, learning_rate=0.001, num_epochs=30, momentum=0.9) # 0.866711
 
-    model = FastTextBasedLSTMModel(index_to_vocab=index_to_vocab)
-    train_model(model, train_set, valid_set, batch_size=32, learning_rate=0.001, num_epochs=30, momentum=0.9) # Train accuracy: 0.8680564895763282, Train loss: 0.3627691795108139 |Validation accuracy: 0.8615819209039548, Validation loss: 0.3758530813840128
+    # model = FastTextBasedLSTMModel(index_to_vocab=index_to_vocab)
+    # train_model(model, train_set, valid_set, batch_size=32, learning_rate=0.001, num_epochs=30, momentum=0.9) # Epoch 30: Train accuracy: 0.9196421407365802, Train loss: 0.264950641202567 |Validation accuracy: 0.8660209846650525, Validation loss: 0.3871996518104307
 
-
-    # baseline_model = ToxicBaseLSTM()
-    # saved_model_path = get_model_name(baseline_model.name, 32, 0.001, 0, 0.9)
-    # baseline_model.load_state_dict(torch.load(saved_model_path))
-    #
+    plot_training_curve(get_model_name("FastTextBasedLstmModel", 32, 0.001, 29, 0.9))
 
