8cb3e8f59b302ac1d5dc4ddb7558d65f1f2a5419
clareli9 <li1033889482@gmail.com>
2019-03-28 20:29:51 -0400

    Commit new model glove based lstm with bidirectional

---
+++ model/glove_based_bidirection_lstm.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+from constants import GLOVE_INPUT_SIZE
+import torchtext
+
+class GloveBasedBidirectionalLSTMModel(nn.Module):
+    def __init__(self, index_to_vocab=None, input_size=GLOVE_INPUT_SIZE, hidden_size=GLOVE_INPUT_SIZE, num_classes=3):
+        super(GloveBasedBidirectionalLSTMModel, self).__init__()
+        self.index_to_vocab = index_to_vocab
+        self.glove = torchtext.vocab.GloVe(name="6B", dim=GLOVE_INPUT_SIZE)  # TODO tune number
+        self.hidden_size = hidden_size
+        self.name = 'GloveBasedBidirectionalLstmModel'
+        self.rnn = nn.LSTM(input_size, hidden_size, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(hidden_size * 2, num_classes)
+
+    def convert_input_to_glove(self, x):
+        if isinstance(x, list):
+            out = torch.zeros(1, len(x), GLOVE_INPUT_SIZE)
+            for i in range(len(x)):
+                out[0][i] = self.glove[x[i]]
+        else:
+            out = torch.zeros(x.shape[0], x.shape[1], GLOVE_INPUT_SIZE)
+            for i in range(x.shape[0]):
+                for j in range(x.shape[1]):
+                    out[i][j] = self.glove[self.index_to_vocab[x[i][j]]]
+
+        return out
+
+    def forward(self, x):
+        x = self.convert_input_to_glove(x)
+        # Need to call the function "convert_word_to_glove"
+        h0 = torch.zeros(2, x.size(0), self.hidden_size)
+        c0 = torch.zeros(2, x.size(0), self.hidden_size)
+        # Forward propagate the LSTM
+        out, _ = self.rnn(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+
+    def canProcess(self, token):
+        return not torch.equal(self.glove[token], torch.zeros(GLOVE_INPUT_SIZE))
